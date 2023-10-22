063ac4486c7c288bbd6ed88e8ac84b194899769d
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-10-22 16:08:04 +0900

    追加した内容の配列を作成

--- src/components/InputTask.tsx
+++ src/components/InputTask.tsx
@@ -1,16 +1,22 @@
 import "./InputTask.css"
 import {useRecoilState, useRecoilValue, useSetRecoilState} from "recoil";
 import {inputTitleState} from "../states/inputTitleState";
+import {addTitleState} from "../states/addTitleState"
 import React, {useCallback} from "react";
 export const InputTask = () => {
+
+    const getKey = () => Math.random().toString(32).substring(2)
 
     // const inputTitle = useRecoilValue(inputTitleState);
     // const setInputTitle = useSetRecoilState(inputTitleState);
 
     const [inputTitle, setInputTitle] = useRecoilState(inputTitleState);
+    const [addTitle, setAddtitle] = useRecoilState(addTitleState);
 
     const handleClick = () => {
-        console.log("inputTitle");
+        setAddtitle([...addTitle,{ id: getKey(), title: inputTitle}])
+        setInputTitle("");
+        console.log(addTitle);
     }
 
     const onChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
@@ -19,7 +25,7 @@ export const InputTask = () => {
     }, [inputTitle]);
 
     return <div className="inputField">
-        <input type="text" className="inputTitle" onChange={onChange}/>
+        <input type="text" className="inputTitle" onChange={onChange} value={inputTitle}/>
         <button type="button" className="addButton" onClick={handleClick}>追加</button>
     </div>
 };
