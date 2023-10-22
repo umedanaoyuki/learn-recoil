86f2a05dfdd46b328817eaeedfd8502fd09a4d6c
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-10-22 15:23:49 +0900

    recoilの設定

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 import {InputTask} from "./components/InputTask";
 import {AddTask} from "./components/AddTask";
+import {RecoilRoot} from "recoil";
 
 function App() {
   return (
-    <div className="App">
-        <InputTask />
-        <AddTask />
-    </div>
+      <RecoilRoot>
+             <div className="App">
+                <InputTask />
+                <AddTask />
+             </div>
+      </RecoilRoot>
   );
 }
 
