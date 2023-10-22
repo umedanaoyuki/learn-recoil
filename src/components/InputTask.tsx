20a88bd5fec5de075a30df837ce9b8d3d2dd07dd
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-10-22 15:27:37 +0900

    atomからvalueをとってくる

--- src/components/InputTask.tsx
+++ src/components/InputTask.tsx
@@ -1,7 +1,16 @@
 import "./InputTask.css"
+import {useRecoilValue} from "recoil";
+import {inputTitleState} from "../states/inputTitleState";
 export const InputTask = () => {
+
+    const inputTitle = useRecoilValue(inputTitleState);
+
+    const handleClick = () => {
+        console.log("inputTitle");
+    }
+
     return <div className="inputField">
         <input type="text" className="inputTitle" />
-        <button type="button" className="addButton">追加</button>
+        <button type="button" className="addButton" onClick={handleClick}>追加</button>
     </div>
 };
--- src/states/inputTitleState.ts
+++ src/states/inputTitleState.ts
@@ -2,5 +2,5 @@ import { atom } from "recoil";
 
 export const inputTitleState = atom<string>({
     key: "inputTitleState",
-    default: "",
+    default: "first inputTitle",
 })
